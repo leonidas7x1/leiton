cc9eba7cb5d8ae511b2c4a42cee9bd2985fd7897
leonidas7x1 <leiton28@gmail.com>
2025-01-08 20:38:28 -0300

    ejercicios for

---
+++ for.py
@@ -0,0 +1,29 @@
+# num=0
+# promedio=0
+# for i in range(3):
+#     num = int(input("ingrese numeros"))
+#     promedio=promedio+num
+#      print("su promedio es:",promedio /3)
+# num=9
+# if num%2==0:
+#     print("el numero es par")
+# else:
+#     print("el numero es impar")  
+# #todos los precios son pares
+# num=0
+# resultado=0
+# num=int(input("ingrese un numero:"))
+# for i in range(num):
+#     resultado=resultado+i
+    
+    
+#     if num%1==0:
+#         print("el numero es par",resultado)
+           
+#     else:
+#         print("el numero es impar",resultado)
+         
+num =int(input("ingrese un numero:"))
+for i in range(num):
+    if i%2!=0:
+        print("el numero no es par",i)   
